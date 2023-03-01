93423e23d61ea15674e6eaa992d4560e9f8a8140
Xavier <xmtrinidad@gmail.com>
2023-02-28 22:16:10 -0700

    started comments card

---
+++ src/lib/components/Comment.svelte
@@ -0,0 +1,84 @@
+<script lang="ts"></script>
+
+<div class="comment-container">
+  <div class="desktop-voter">
+    <span>Voter</span>
+  </div>
+
+  <div class="comment-header-body-container">
+    <div class="comment-header">
+      <div class="comment-header-avatar-container">
+        <div class="comment-header-avatar"></div>
+        <h2 class="comment-header-username">Meme</h2>
+      </div>
+      <span class="comment-header-time">1 month ago</span>
+    </div>
+    <div class="comment-body">
+      <p>Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed the design and the responsiveness at various breakpoints works really well.</p>
+    </div>
+    <div class="comment-footer">
+      <span>Voter</span>
+      <button>Reply</button>
+    </div>
+  </div>
+  
+</div>
+
+<style>
+  .desktop-voter {
+    display: none;
+  }
+
+  .comment-header-body-container {
+    padding: 8px 12px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+
+  .comment-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .comment-header-avatar-container {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+  }
+
+  .comment-header-avatar {
+    width: 32px;
+    height: 32px;
+    border-radius: 100%;
+    background-color: hotpink;
+  }
+
+  .comment-header-time {
+    color: #67727E;
+  }
+
+  .comment-header-username {
+    font-size: 1rem;
+  }
+
+  .comment-body p {
+    line-height: 24px;
+  }
+
+  .comment-footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .comment-footer button {
+    background-color: transparent;
+    color: #5357B6;
+    width: 66px;
+    height: 24px;
+    border-radius: 4px;
+    font-weight: 600;
+  }
+</style>
--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
-    import NoComments from "$lib/components/NoComments.svelte";
-
+  import Comment from "$lib/components/Comment.svelte";
+import NoComments from "$lib/components/NoComments.svelte";
+  const topics = ['1'];
 </script>
 
 
 
-<NoComments />
+{#if topics.length === 0}
+  <NoComments />
+{:else}
+  <div class="topics-container">
+    <Comment />
+  </div>
+{/if}
+
+<style>
+  .topics-container {
+    margin: 24px 12px;
+    background-color: #fff;
+    border-radius: 8px;
+  }
+</style>
